eeaba9e7e3c2f1d917cf1cd6e564f1ef420cfdc0
patrick brisbin <pbrisbin@gmail.com>
2021-05-18 15:37:52 -0400

    Refactor and support direct use as GitHub Action
    - Fix manifest location for new docs Stack
    - Simplify image handling to make CI easier
    - Remove Lint

--- restylers/app/Main.hs
+++ restylers/app/Main.hs
@@ -1,14 +1,14 @@
 module Main
     ( main
-    )
-where
+    ) where
 
 import RIO
 
+import RIO.Directory (doesFileExist)
+import RIO.FilePath (takeExtension, (<.>), (</>))
 import Restylers.App
 import Restylers.Build
 import qualified Restylers.Info.Resolved as Info
-import Restylers.Lint
 import Restylers.Manifest (toRestyler)
 import qualified Restylers.Manifest as Manifest
 import Restylers.Options
@@ -22,22 +22,36 @@ main = do
         app <- loadApp opts lf
         runRIO app $ do
             logDebug $ "Options: " <> displayShow opts
+            yaml <- locateYaml oInput
+            info <- Info.load yaml
 
-            restylers <- for oInputs $ \yaml -> do
-                logDebug $ "Input: " <> fromString yaml
-                info <- Info.load yaml
-                image <- getRestylerImage info
-                logInfo $ "Restyler image: " <> display image
+            when oBuild $ buildRestylerImage info
+
+            image <- tagRestylerImage info
+            testRestylerImage info image
+
+            when oPush $ do
                 exists <- doesRestylerImageExist image
+                if exists
+                    then logWarn "Not pushing, image exists"
+                    else pushRestylerImage image
 
-                if exists && not oAlways
-                    then logInfo "Image exists, skipping"
-                    else do
-                        lintRestyler info
-                        buildRestylerImage info image
-                        testRestylerImage info image
-                        when oPush $ pushRestylerImage image
-
-                pure $ toRestyler info image
-
+            let restylers = pure $ toRestyler info image
             traverse_ (liftIO . (`Manifest.write` restylers)) oWrite
+
+locateYaml
+    :: (MonadIO m, MonadReader env m, HasLogFunc env) => FilePath -> m FilePath
+locateYaml input
+    | takeExtension input `elem` [".yml", ".yaml"] = pure input
+    | otherwise = do
+        let input' = input </> "info" <.> "yaml"
+        exists <- doesFileExist input'
+        input' <$ unless exists err
+  where
+    err :: (MonadIO m, MonadReader env m, HasLogFunc env) => m ()
+    err = do
+        logError
+            $ "Invalid PATH input ("
+            <> fromString input
+            <> "). Must be .yml, .yaml, or a directory containing an info.yaml"
+        exitFailure
--- restylers/src/Restylers/Lint.hs
+++ restylers/src/Restylers/Lint.hs
@@ -1,19 +1,18 @@
+-- Unused. Keeping just to avoid busting package.yaml docker cache :grimace:
 module Restylers.Lint
     ( lintRestyler
-    )
-where
+    ) where
 
 import RIO
 
 import Data.Aeson
-import qualified Restylers.Info.Build as Build
-import Restylers.Info.Resolved (ImageSource(..), RestylerInfo)
-import qualified Restylers.Info.Resolved as Info
-import RIO.Directory (getCurrentDirectory)
 import RIO.FilePath ((</>))
 import RIO.Process
 import qualified RIO.Text as T
-import System.Environment (lookupEnv)
+import Restylers.Directory
+import qualified Restylers.Info.Build as Build
+import Restylers.Info.Resolved (ImageSource(..), RestylerInfo)
+import qualified Restylers.Info.Resolved as Info
 
 data LintError = LintError
     { code :: Text
@@ -66,17 +65,14 @@ lintDockerfile
 lintDockerfile dockerfile = do
     logInfo $ "Linting " <> fromString dockerfile
 
-    -- On CI, we are docker-within-docker, which means our PWD is "/code", but
-    -- we need to mount these files using the non-containerized PWD, which we
-    -- can (only) figure out if we're told (e.g. via ENV)
-    cwd <- maybe getCurrentDirectory pure =<< liftIO (lookupEnv "REALPWD")
+    chd <- getCurrentHostDirectory
 
-    (ec, bs) <- proc
+    (_, bs) <- proc
         "docker"
         (concat
             [ ["run", "--rm"]
-            , ["--volume", cwd </> ".hadolint.yaml:/config.yaml:ro"]
-            , ["--volume", cwd </> dockerfile <> ":/Dockerfile:ro"]
+            , ["--volume", chd </> ".hadolint.yaml:/config.yaml:ro"]
+            , ["--volume", chd </> dockerfile <> ":/Dockerfile:ro"]
             , ["hadolint/hadolint", "hadolint"]
             , ["--config", "/config.yaml"]
             , ["--format", "json"]
@@ -85,16 +81,12 @@ lintDockerfile dockerfile = do
         )
         readProcessStdout
 
-    logDebug $ "hadolint exit code: " <> displayShow ec
     logDebug $ "hadolint stdout: " <> displayShow bs
 
     case eitherDecode bs of
         Left err -> throwString $ "Unable to read hadolint output: " <> err
+        Right [] -> pure ()
         Right (errors :: [LintError]) -> do
-            unless (null errors)
-                $ logError
-                $ "Lint errors found in "
-                <> fromString dockerfile
+            logError $ "Lint errors found in " <> fromString dockerfile
             traverse_ (logError . display) errors
-
-    when (ec /= ExitSuccess) $ exitWith ec
+            exitFailure
